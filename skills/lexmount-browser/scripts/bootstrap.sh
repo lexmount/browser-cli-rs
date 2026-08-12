20abeecdfdd6c6785ea3b40c1547c60c2f6c32c1
freelw <freelw81@qq.com>
2026-08-12 12:06:37 +0800

    chore: prepare browser-cli v0.1.5

--- skills/lexmount-browser/scripts/bootstrap.sh
+++ skills/lexmount-browser/scripts/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-version="${LEXMOUNT_BROWSER_CLI_VERSION:-0.1.4}"
+version="${LEXMOUNT_BROWSER_CLI_VERSION:-0.1.5}"
 repo="https://github.com/lexmount/browser-cli-rs/releases/download/v${version}"
 case "$(uname -s)-$(uname -m)" in
   Darwin-arm64) target="aarch64-apple-darwin" ;;
